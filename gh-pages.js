49367b26ba104545d10bb83d1d5483c20a7c6750
elsadig-ahmed <alsadq.ahmd@gmail.com>
2022-06-10 09:56:29 +0200

    add gh-page

---
+++ gh-pages.js
@@ -0,0 +1,18 @@
+import { publish } from 'gh-pages';
+
+publish(
+ 'build', // path to public directory
+ {
+  branch: 'gh-pages',
+  user: {
+      //   repo: 'https://github.com/el3um4s/memento-sveltekit-and-github-pages.git', // Update to point to your repository
+    repo: 'https://github.com/Alsadig-ahmed/sveltekit-gh-pages.git', // Update to point to your repository
+    name: 'Alsadig-ahmed', 
+    email: 'alsadq.ahmed@gmail.com' 
+  },
+  dotfiles: true
+  },
+  () => {
+   console.log('Deploy Complete!');
+  }
+);
